c7c48094395ee2b6a72e967d8dcafefdb8dea8d1
Gena <g@goodworkapps.com>
2015-07-22 22:14:12 +0300

    Adds carthage support

---
+++ SwiftOverlays/SwiftOverlays.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOverlays.h
+//  SwiftOverlays
+//
+//  Created by Gennady Evstratov on 22.07.15.
+//  Copyright (c) 2015 Synqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOverlays.
+FOUNDATION_EXPORT double SwiftOverlaysVersionNumber;
+
+//! Project version string for SwiftOverlays.
+FOUNDATION_EXPORT const unsigned char SwiftOverlaysVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOverlays/PublicHeader.h>
+
+
